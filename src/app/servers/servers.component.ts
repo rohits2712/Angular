7cfff35a36e806fda645899d835cbcaadc765165
Rohit Sharma <rohitrises@gmail.com>
2019-06-03 04:04:43 +0530

    Fix for bootstrap and check with tags or class as selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',//changing selector as an attribute , now app-server can be an attribute in div
   templateUrl: './servers.component.html',//` can be used for multiline comment
   styleUrls: ['./servers.component.css']
 })
