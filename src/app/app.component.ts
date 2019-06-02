5ea87b13d69f16bf47d264aa749f703946b552a9
Rohit Sharma <rohitrises@gmail.com>
2019-06-03 00:36:56 +0530

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] //styleurls can be replaced by styles and inline style  
 })
 export class AppComponent {
   name = '';
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',//` can be used for multiline comment
+  templateUrl: './servers.component.html',//` can be used for multiline comment
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
