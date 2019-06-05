707ac5953d71eddb6e0184ff8ad1c43eedaaed2e
Rohit Sharma <rohitrises@gmail.com>
2019-06-06 03:53:09 +0530

    Passing data events from user

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-
+  serverName="";
   constructor() { 
       setTimeout(() => {
         this.allowNewServer = true;
@@ -20,5 +20,10 @@ export class ServersComponent implements OnInit {
   }
   OnCreateServer(){
     this.serverCreationStatus = "Server was created";
+  }
+
+  onUpdateServerName(event : Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+
   }
 }
