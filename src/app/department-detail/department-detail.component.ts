54ffc554292c41ebda8538e0a147f3810f059e90
Rohit Sharma <rohitrises@gmail.com>
2019-07-21 00:44:10 +0530

    Opt params - avoiding changes at different routes

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -40,6 +40,9 @@ export class DepartmentDetailComponent implements OnInit {
   gotoDepartments()
   {
     let selectedId = this.departmentId ?  this.departmentId : null;
-    this.router.navigate(['/departments', {id:selectedId}])
+    // this.router.navigate(['/departments', {id:selectedId}])
+    this.router.navigate(['../',{id:selectedId}],{relativeTo:this.route})
+    //using relativeurl - ../ - remove /1 and go back to department or departmentlist
+    //pass the optional route parameter
   }
 }
